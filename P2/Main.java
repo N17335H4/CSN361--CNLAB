49f361859ff0c5571ab797c8dc4abbe79ee230f9
1341/\/3 /\/|7335|-|4 <bniteesha@cs.iitr.ac.in>
2021-03-25 22:53:32 +0530

    ping url left

--- P2/Main.java
+++ P2/Main.java
@@ -3,6 +3,28 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+/*
+import java.io.*;
+import java.net.*;
+
+public class ReachableTest {
+ public static void main(String args[]) {
+     try {
+       InetAddress address = InetAddress.getByName("web.mit.edu");
+       System.out.println("Name: " + address.getHostName());
+       System.out.println("Addr: " + address.getHostAddress());
+       System.out.println("Reach: " + address.isReachable(3000));
+     }
+     catch (UnknownHostException e) {
+       System.err.println("Unable to lookup web.mit.edu");
+     }
+     catch (IOException e) {
+       System.err.println("Unable to reach web.mit.edu");
+     }
+   }
+}
+*/
+
 class Main
 {
 // Sends ping request to a provided IP address
