7405bd9f94d5f0ae9c240266a5cc852692f6a067
Niteesha-Balne <niteeshabalne@gmail.com>
2021-04-15 21:55:36 +0530

    MAC added

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+using namespace std;
 
 
 // Returns hostname for the local computer
@@ -40,6 +43,23 @@ void checkIPbuffer(char *IPbuffer)
 	}
 }
 
+string GetStdoutFromCommand(string cmd) {
+
+  string data;
+  FILE * stream;
+  const int max_buffer = 256;
+  char buffer[max_buffer];
+  cmd.append(" 2>&1");
+
+  stream = popen(cmd.c_str(), "r");
+
+  if (stream) {
+    while (!feof(stream))
+      if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+    pclose(stream);
+  }
+  return data;
+}
 
 
 // Driver code
@@ -49,6 +69,8 @@ int main()
 	char *IPbuffer;
 	struct hostent *host_entry;
 	int hostname;
+	ofstream fout;
+	fout.open("sample.txt");
 
 	// To retrieve hostname
 	hostname = gethostname(hostbuffer, sizeof(hostbuffer));
@@ -63,8 +85,35 @@ int main()
 	IPbuffer = inet_ntoa(*((struct in_addr*)
 						host_entry->h_addr_list[0]));
 
-	printf("Hostname: %s\n", hostbuffer);
-	printf("Host IP: %s\n", IPbuffer);
+	string data = GetStdoutFromCommand("ifconfig");
+	string conf;
+	fout << data << endl;
+	fout.close();
+    ifstream fin;
+    fin.open("sample.txt");
+    int i=0;
+    while (i<2) {
+    	getline(fin, data);
+    	conf = data;  
+    	i++;
+    }
+
+	char mac[17];
+	for (int i = 0; i < 17; ++i)
+	{
+		mac[i] = conf[i+14];
+	}
+    fin.close();
+  
+    printf("MAC Address: ");
+	for (int i = 0; i < 17; ++i)
+	{
+		cout << mac[i];
+	}
+	cout << endl;
+	
+	printf("Host Name: %s\n", hostbuffer);
+	printf("IP Address: %s\n", IPbuffer);
 
 	return 0;
 }
