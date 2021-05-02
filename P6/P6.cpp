b72ca62cf5b36e0761173f59c967f22c56af89e9
Niteesha-Balne <niteeshabalne@gmail.com>
2021-05-02 22:31:40 +0530

    possible value and format errors handled

--- P6/P6.cpp
+++ P6/P6.cpp
@@ -3,6 +3,34 @@
 #include <stdio.h> 
 #include <string>
 using namespace std;
+int numcheck(char n){
+	int c=0;
+	switch(n){
+		case '0':
+			c=1;
+		case '1':
+			c=1;
+		case '2':
+			c=1;
+		case '3':
+			c=1;
+		case '4':
+			c=1;
+		case '5':
+			c=1;
+		case '6':
+			c=1;
+		case '7':
+			c=1;
+		case '8':
+			c=1;
+		case '9':
+			c=1;
+	}
+	if (c==0)
+		cout << "Invalid value entered" << endl;
+	return c;
+}
 
 int main()
 {
@@ -15,6 +43,11 @@ int main()
 	last = power.length();
 	if(power[last-1]=='W' && power[last-2]=='B')
 	{
+		for (int i = 0; i < last-3; ++i)
+		{
+			if(numcheck(power[i])==0)
+				return 0;
+		}
 		val = power.substr(0,last-3);
 		num = stod(val) + 30;
 		num /= 10;
@@ -23,6 +56,11 @@ int main()
 	}
 	else if (power[last-1]=='W')
 	{
+		for (int i = 0; i < last-1; ++i)
+		{
+			if(numcheck(power[i])==0)
+				return 0;
+		}
 		val = power.substr(0,last-1);
 		num = stod(val);
 		num = 10 * log10(num/0.001);
@@ -30,6 +68,11 @@ int main()
 	}
 	else if (power[last-1]=='m')
 	{
+		for (int i = 0; i < last-3; ++i)
+		{
+			if(numcheck(power[i])==0)
+				return 0;
+		}
 		val = power.substr(0,last-3);
 		num = stod(val);
 		num /= 10;
